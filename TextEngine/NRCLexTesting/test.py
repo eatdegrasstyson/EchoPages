b7897fd326dbd82b4329f6b3448ecd14fdb7b6f5
Ryan Thomas <ryanthomasus@outlook.com>
2026-02-06 17:28:24 -0500

    test file added and more tests done

--- TextEngine/NRCLexTesting/test.py
+++ TextEngine/NRCLexTesting/test.py
@@ -1,54 +1,18 @@
 from nrclex import NRCLex
 
-text1 = "I feel excited but also a little nervous about tomorrow. I love learning new things."
-text = 'I was so stressed last night because I was studying all day for my test in one hour. I was crying all day because of stress. After studying and taking the test though, I feel really good and happy with how I did. I feel like I a burden was lifted. Now I am super duper happy!'
-
-'''
-I was so stressed last night because I was studying all day for my test in one hour. 
-I was crying all day because of stress. 
-After studying and taking the test though, I feel really good and happy with how I did. 
-I feel like I a burden was lifted. Now I am super duper happy!
-
-Affect frequencies:
-fear: 0.0
-anger: 0.0
-anticip: 0.0
-trust: 0.1875
-surprise: 0.0625
-positive: 0.1875
-negative: 0.125
-sadness: 0.0625
-disgust: 0.0
-joy: 0.1875
-anticipation: 0.1875
-
-Raw emotion scores:
-negative: 2
-sadness: 1
-anticipation: 3
-joy: 3
-positive: 3
-surprise: 1
-trust: 3
-
-Emotion 1: ('trust', 0.1875)
-Emotion 2: ('positive', 0.1875)
-Emotion 3: ('joy', 0.1875)
-Emotion 4: ('anticipation', 0.1875)
-'''
-
+text = "He was looking with fear at his mother as she was coming at him with a dagger in her hand, with an intent to kill him; suddenly he heard a knock, and another knock, and other and suddenly he woke up to the glaring eyes of his teacher who was tapping on his bench, trying to wake him up; little did he know that he was snoring loudly."
 # lex (NRCLex): analyzer object created from text
 lex = NRCLex(text)
 
 print("Affect frequencies:")
 for emotion, score in lex.affect_frequencies.items():
-    print(f"{emotion}: {score}")
+    print(f"\t{emotion}: {score}")
 
 print("\nRaw emotion scores:")
 for emotion, score in lex.raw_emotion_scores.items():
-    print(f"{emotion}: {score}")
+    print(f"\t{emotion}: {score}")
 
-print('\n')
+print("\n")
 count = 0
 for emotion in lex.top_emotions:
     count += 1
