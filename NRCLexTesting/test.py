bfc08d74e70225a2c65857fd5ce59e88efe8e0f6
Ryan Thomas <ryanthomasus@outlook.com>
2026-02-06 17:00:12 -0500

    Tests Added and Formatting

--- NRCLexTesting/test.py
+++ NRCLexTesting/test.py
@@ -1,13 +1,58 @@
 from nrclex import NRCLex
 
-text = "I feel excited but also a little nervous about tomorrow. I love learning new things."
+text1 = "I feel excited but also a little nervous about tomorrow. I love learning new things."
+text = 'I was so stressed last night because I was studying all day for my test in one hour. I was crying all day because of stress. After studying and taking the test though, I feel really good and happy with how I did. I feel like I a burden was lifted. Now I am super duper happy!'
+
+'''
+I was so stressed last night because I was studying all day for my test in one hour. 
+I was crying all day because of stress. 
+After studying and taking the test though, I feel really good and happy with how I did. 
+I feel like I a burden was lifted. Now I am super duper happy!
+
+Affect frequencies:
+fear: 0.0
+anger: 0.0
+anticip: 0.0
+trust: 0.1875
+surprise: 0.0625
+positive: 0.1875
+negative: 0.125
+sadness: 0.0625
+disgust: 0.0
+joy: 0.1875
+anticipation: 0.1875
+
+Raw emotion scores:
+negative: 2
+sadness: 1
+anticipation: 3
+joy: 3
+positive: 3
+surprise: 1
+trust: 3
+
+Emotion 1: ('trust', 0.1875)
+Emotion 2: ('positive', 0.1875)
+Emotion 3: ('joy', 0.1875)
+Emotion 4: ('anticipation', 0.1875)
+'''
 
 # lex (NRCLex): analyzer object created from text
 lex = NRCLex(text)
 
-print("Affect frequencies:", lex.affect_frequencies)
-print("Raw emotion scores:", lex.raw_emotion_scores)
-print("Top emotions:", lex.top_emotions)
+print("Affect frequencies:")
+for emotion, score in lex.affect_frequencies.items():
+    print(f"{emotion}: {score}")
+
+print("\nRaw emotion scores:")
+for emotion, score in lex.raw_emotion_scores.items():
+    print(f"{emotion}: {score}")
+
+print('\n')
+count = 0
+for emotion in lex.top_emotions:
+    count += 1
+    print(f"Emotion {count}: {emotion}")
 
 # Terminal Test Code
 
